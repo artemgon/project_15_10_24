9b54f5e75095149f2bdbbb797562f12b0574ec37
artemgon <artemhoncharuk123@gmail.com>
2024-12-22 18:03:24 +0200

    Task 1 is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,73 @@
 #include "libs.h"
+
+template <class T>
+struct Node
+{
+	T _data;
+	Node<T>* _next;
+	Node<T>* _prev;
+};
+
+template <class T>
+class Queue
+{
+private:
+	Node<T>* _head;
+	Node<T>* _tail;
+	int _size;
+public:
+	Queue() : _head(NULL), _tail(NULL), _size(0) {}
+	~Queue()
+	{
+		while (_head)
+		{
+			Node<T>* temp = _head;
+			_head = _head->_next;
+			delete temp;
+		}
+	}
+	void enqueue(T data)
+	{
+		Node<T>* temp = new Node<T>;
+		temp->_data = data;
+		temp->_next = NULL;
+		temp->_prev = _tail;
+		if (_tail)
+			_tail->_next = temp;
+		_tail = temp;
+		if (!_head)
+			_head = _tail;
+		_size++;
+	}
+	void dequeue()
+	{
+		if (isEmpty())
+		{
+			cout << "Queue is empty" << endl;
+			return;
+		}
+		Node<T>* temp = _head;
+		_head = _head->_next;
+		if (_head == nullptr)
+			_tail = nullptr;
+		delete temp;
+		_size--;
+	}
+	T peek()
+	{
+		if (isEmpty())
+		{
+			cout << "Queue is empty" << endl;
+			return -1;
+		}
+		return _head->_data;
+	}
+	bool isEmpty()
+	{
+		return _size == 0;
+	}
+	int getSize()
+	{
+		return _size;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+    Queue<int> q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    cout << "Front element: " << q.peek() << endl;
+    q.dequeue();
+    cout << "Front element after dequeue: " << q.peek() << endl;
+    cout << "Queue size: " << q.getSize() << endl;
 	return 0;
 }
