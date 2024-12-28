9cfbbcf5ce13e2cb3df6d15252152c30882ff880
artemgon <artemhoncharuk123@gmail.com>
2024-12-28 16:22:42 +0200

    Task 2 is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,130 @@
 #include "libs.h"
+
+template <class T>
+struct Node
+{
+	T _data;
+	Node<T>* _next;
+	Node<T>* _prev;
+};
+
+template <class T>
+class Queue
+{
+private:
+	Node<T>* _head;
+	Node<T>* _tail;
+	int _size;
+public:
+	Queue() : _head(NULL), _tail(NULL), _size(0) {}
+	~Queue()
+	{
+		while (_head)
+		{
+			Node<T>* temp = _head;
+			_head = _head->_next;
+			delete temp;
+		}
+	}
+	void enqueue(T data)
+	{
+		Node<T>* temp = new Node<T>;
+		temp->_data = data;
+		temp->_next = NULL;
+		temp->_prev = _tail;
+		if (_tail)
+			_tail->_next = temp;
+		_tail = temp;
+		if (!_head)
+			_head = _tail;
+		_size++;
+	}
+	void dequeue()
+	{
+		if (isEmpty())
+		{
+			cout << "Queue is empty" << endl;
+			return;
+		}
+		Node<T>* temp = _head;
+		_head = _head->_next;
+		if (_head == nullptr)
+			_tail = nullptr;
+		delete temp;
+		_size--;
+	}
+	T peek()
+	{
+		if (isEmpty())
+		{
+			cout << "Queue is empty" << endl;
+			return -1;
+		}
+		return _head->_data;
+	}
+	Node<T>* clone(Queue<T>& q) const
+	{
+		Node<T>* temp = _head;
+		while (temp)
+		{
+			q.enqueue(temp->_data);
+			temp = temp->_next;
+		}
+		return q._head;
+	}
+	Node<T>* operator+(Queue<T>& q) const
+	{
+		Queue<T>* temp = new Queue<T>;
+		Node<T>* tempNode = _head;
+		while (tempNode)
+		{
+			temp->enqueue(tempNode->_data);
+			tempNode = tempNode->_next;
+		}
+		tempNode = q._head;
+		while (tempNode)
+		{
+			temp->enqueue(tempNode->_data);
+			tempNode = tempNode->_next;
+		}
+		return temp->_head;
+	};
+	Node<T>* operator*(Queue<T>& q) const
+	{
+		Queue<T>* temp = new Queue<T>;
+		Node<T>* tempNode = _head;
+		while (tempNode)
+		{
+			Node<T>* tempNode2 = q._head;
+			while (tempNode2)
+			{
+				if (tempNode->_data == tempNode2->_data)
+				{
+					temp->enqueue(tempNode->_data);
+					break;
+				}
+				tempNode2 = tempNode2->_next;
+			}
+			tempNode = tempNode->_next;
+		}
+		return temp->_head;
+	}
+	void print() const
+	{
+		Node<T>* temp = _head;
+		while (temp)
+		{
+			cout << temp->_data << " ";
+			temp = temp->_next;
+		}
+		cout << endl;
+	}
+	bool isEmpty()
+	{
+		return _size == 0;
+	}
+	int getSize()
+	{
+		return _size;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,43 @@
 
 int main()
 {
+	//Test
+	Queue<int> q1, q2, q3, q4, q5;
+	q1.enqueue(10);
+	q1.enqueue(20);
+	q1.enqueue(30);
+	q1.enqueue(40);
+	q1.enqueue(50);
+	Node<int>* pointer = q1.clone(q2);
+	while (pointer)
+	{
+		cout << pointer->_data << " ";
+		pointer = pointer->_next;
+	}
+	cout << endl;
+	q3.enqueue(60);
+	q3.enqueue(70);
+	q3.enqueue(80);
+	q3.enqueue(90);
+	q3.enqueue(100);
+	Node<int>* pointer2 = q2 + q3;
+	while (pointer2)
+	{
+		cout << pointer2->_data << " ";
+		pointer2 = pointer2->_next;
+	}
+	cout << endl;
+	Node<int>* pointer3 = q1.clone(q4);
+	q5.enqueue(30);
+	q5.enqueue(40);
+	q5.enqueue(50);
+	q5.enqueue(60);
+	q5.enqueue(70);
+	Node<int>* pointer4 = q4 * q5;
+	while (pointer4)
+	{
+		cout << pointer4->_data << " ";
+		pointer4 = pointer4->_next;
+	}
 	return 0;
 }
