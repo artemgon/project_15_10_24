faa86f5f74b282495d0cb949f2e10df0c49ee582
artemgon <artemhoncharuk123@gmail.com>
2024-12-30 01:22:33 +0200

    HW is done.

--- classes.h
+++ classes.h
@@ -1,23 +1,59 @@
 #include "libs.h"
 
-template <class T>
-struct Node
-{
+template <typename T>
+struct Node {
 	T _data;
 	Node<T>* _next;
 	Node<T>* _prev;
+	Node(T data) : _data(data), _next(nullptr), _prev(nullptr) {}
 };
 
-template <class T>
-class Queue
+template <typename T>
+class Array
 {
 private:
 	Node<T>* _head;
 	Node<T>* _tail;
-	int _size;
+	int _size, _capacity, _grow;
 public:
-	Queue() : _head(NULL), _tail(NULL), _size(0) {}
-	~Queue()
+	Array(int initSize, int growBy = 1) : _head(nullptr), _tail(nullptr), _size(0), _capacity(initSize), _grow(growBy)
+	{
+		if (growBy <= 0)
+		{
+			cout << "Error.Invalid grow value..." << endl;
+			exit(1);
+		}
+	}
+	int GetSize()  const { return _capacity; }
+	void SetSize(int size, int grow = 1) {
+		if (grow <= 0) {
+			cout << "Error. Invalid grow value..." << endl;
+			exit(1);
+		}
+		if (size < _size) {
+			int removeCount = _size - size;
+			for (int i = 0; i < removeCount; ++i) {
+				RemoveAt(_size - 1);
+			}
+		}
+		else if (size > _size) {
+			for (int i = _size; i < size; ++i) {
+				Add(T());  
+			}
+		}
+		_capacity = size;
+		_grow = grow;
+	}
+	int GetUpperBound() const { return _size - 1; }
+	bool IsEmpty() const { return _size == 0; }
+	void FreeExtra()
+	{
+		if (_size < _capacity)
+		{
+			_capacity = _size;
+		}
+	}
+	void RemoveAll()
 	{
 		while (_head)
 		{
@@ -25,106 +61,147 @@ public:
 			_head = _head->_next;
 			delete temp;
 		}
+		_head = _tail = nullptr;
+		_size = 0;
+		_capacity = 0;
 	}
-	void enqueue(T data)
+	T GetAt(int index) const {
+		if (index < 0 || index >= _size) {
+			cout << "Error. Invalid index..." << endl;
+			exit(1);
+		}
+		Node<T>* temp = _head;
+		for (int i = 0; i < index; i++) {
+			temp = temp->_next;
+		}
+		return temp->_data;
+	}
+	void SetAt(int index, T data)
 	{
-		Node<T>* temp = new Node<T>;
+		if (index < 0 || index >= _size)
+		{
+			cout << "Error.Invalid index..." << endl;
+			exit(1);
+		}
+		Node<T>* temp = _head;
+		for (int i = 0; i < index; i++)
+		{
+			temp = temp->_next;
+		}
 		temp->_data = data;
-		temp->_next = NULL;
-		temp->_prev = _tail;
-		if (_tail)
-			_tail->_next = temp;
-		_tail = temp;
+	}
+	T& operator[](int index) {
+		if (index < 0 || index >= _size) {
+			cout << "Error. Invalid index..." << endl;
+			exit(1);
+		}
+		Node<T>* temp = _head;
+		for (int i = 0; i < index; i++) {
+			temp = temp->_next;
+		}
+		return temp->_data;
+	}
+	void Add(T data)
+	{
+		if (_size >= _capacity)
+		{
+			_capacity += _grow;
+		}
+		Node<T>* newNode = new Node<T>(data);
 		if (!_head)
-			_head = _tail;
+		{
+			_head = _tail = newNode;
+		}
+		else
+		{
+			_tail->_next = newNode;
+			newNode->_prev = _tail;
+			_tail = newNode;
+		}
 		_size++;
 	}
-	void dequeue()
+	void Append(const Array<T>& other)
 	{
-		if (isEmpty())
+		Node<T>* current = other._head;
+		while (current)
 		{
-			cout << "Queue is empty" << endl;
-			return;
+			Add(current->_data);
+			current = current->_next;
 		}
-		Node<T>* temp = _head;
-		_head = _head->_next;
-		if (_head == nullptr)
-			_tail = nullptr;
-		delete temp;
+	}
+	Array<T>& operator = (const Array<T>& other)
+	{
+		if (this == &other)
+		{
+			return *this;
+		}
+		RemoveAll();
+		Node <T>* current = other._head;
+		while (current)
+		{
+			Add(current->_data);
+			current = current->_next;
+		}
+		return *this;
+	}
+	T* GetData() const
+	{
+		T* arr = new T[_size];
+		Node<T>* current = _head;
+		for (int i = 0; i < _size; i++)
+		{
+			arr[i] = current->_data;
+			current = current->_next;
+		}
+		return arr;
+	}
+	void InsertAt(int index, T value) {
+		if (index < 0 || index > _size) {  
+			cout << "Error. Invalid index..." << endl;
+			exit(1);
+		}
+		Node<T>* newNode = new Node<T>(value);
+		if (index == 0) {  
+			newNode->_next = _head;
+			if (_head) _head->_prev = newNode;
+			_head = newNode;
+			if (!_tail) _tail = _head; 
+		}
+		else if (index == _size) { 
+			newNode->_prev = _tail;
+			if (_tail) _tail->_next = newNode;
+			_tail = newNode;
+		}
+		else {  
+			Node<T>* current = _head;
+			for (int i = 0; i < index - 1; i++) {
+				current = current->_next;
+			}
+			newNode->_next = current->_next;
+			newNode->_prev = current;
+			if (current->_next) current->_next->_prev = newNode;
+			current->_next = newNode;
+		}
+		_size++;
+	}
+	void RemoveAt(int index)
+	{
+		if (index < 0 || index >= _size)
+		{
+			cout << "Error.Invalid index..." << endl;
+			exit(1);
+		}
+		Node<T>* current = _head;
+		for (int i = 0; i < index; i++)
+		{
+			current = current->_next;
+		}
+		if (current->_prev) current->_prev->_next = current->_next;
+		if (current->_next) current->_next->_prev = current->_prev;
+		if (current == _head) _head = current->_next;
+		if (current == _tail) _tail = current->_prev;
+		delete current;
 		_size--;
 	}
-	T peek()
-	{
-		if (isEmpty())
-		{
-			cout << "Queue is empty" << endl;
-			return -1;
-		}
-		return _head->_data;
-	}
-	Node<T>* clone(Queue<T>& q) const
-	{
-		Node<T>* temp = _head;
-		while (temp)
-		{
-			q.enqueue(temp->_data);
-			temp = temp->_next;
-		}
-		return q._head;
-	}
-	Node<T>* operator+(Queue<T>& q) const
-	{
-		Queue<T>* temp = new Queue<T>;
-		Node<T>* tempNode = _head;
-		while (tempNode)
-		{
-			temp->enqueue(tempNode->_data);
-			tempNode = tempNode->_next;
-		}
-		tempNode = q._head;
-		while (tempNode)
-		{
-			temp->enqueue(tempNode->_data);
-			tempNode = tempNode->_next;
-		}
-		return temp->_head;
-	};
-	Node<T>* operator*(Queue<T>& q) const
-	{
-		Queue<T>* temp = new Queue<T>;
-		Node<T>* tempNode = _head;
-		while (tempNode)
-		{
-			Node<T>* tempNode2 = q._head;
-			while (tempNode2)
-			{
-				if (tempNode->_data == tempNode2->_data)
-				{
-					temp->enqueue(tempNode->_data);
-					break;
-				}
-				tempNode2 = tempNode2->_next;
-			}
-			tempNode = tempNode->_next;
-		}
-		return temp->_head;
-	}
-	void print() const
-	{
-		Node<T>* temp = _head;
-		while (temp)
-		{
-			cout << temp->_data << " ";
-			temp = temp->_next;
-		}
-		cout << endl;
-	}
-	bool isEmpty()
-	{
-		return _size == 0;
-	}
-	int getSize()
-	{
-		return _size;
-	}
+	~Array() { RemoveAll(); }
 };
--- main.cpp
+++ main.cpp
@@ -2,43 +2,27 @@
 
 int main()
 {
-	//Test
-	Queue<int> q1, q2, q3, q4, q5;
-	q1.enqueue(10);
-	q1.enqueue(20);
-	q1.enqueue(30);
-	q1.enqueue(40);
-	q1.enqueue(50);
-	Node<int>* pointer = q1.clone(q2);
-	while (pointer)
-	{
-		cout << pointer->_data << " ";
-		pointer = pointer->_next;
-	}
-	cout << endl;
-	q3.enqueue(60);
-	q3.enqueue(70);
-	q3.enqueue(80);
-	q3.enqueue(90);
-	q3.enqueue(100);
-	Node<int>* pointer2 = q2 + q3;
-	while (pointer2)
-	{
-		cout << pointer2->_data << " ";
-		pointer2 = pointer2->_next;
-	}
-	cout << endl;
-	Node<int>* pointer3 = q1.clone(q4);
-	q5.enqueue(30);
-	q5.enqueue(40);
-	q5.enqueue(50);
-	q5.enqueue(60);
-	q5.enqueue(70);
-	Node<int>* pointer4 = q4 * q5;
-	while (pointer4)
-	{
-		cout << pointer4->_data << " ";
-		pointer4 = pointer4->_next;
-	}
+    Array<int> arr(5, 2);
+    arr.Add(10);
+    arr.Add(20);
+    arr.Add(30);
+    arr.Add(40);
+    cout << "Initial Array: ";
+    for (int i = 0; i < arr.GetSize(); ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    arr.InsertAt(2, 25);
+    cout << "After InsertAt(2, 25): ";
+    for (int i = 0; i < arr.GetSize(); ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    arr.RemoveAt(1);
+    cout << "After RemoveAt(1): ";
+    for (int i = 0; i < arr.GetSize(); ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 	return 0;
 }
